9602fc95bab54666029a86c3f681f924db986f79
reizyte <112692614+reizyte@users.noreply.github.com>
2022-09-17 09:00:42 +0000

    9/17-header

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  
-  
+   get "homes/about" => "homes#about",as:'about'
   root to: 'homes#top'
   devise_for :users
-  resources :bookers, only: [:new, :index, :show, :create, :destroy]
-  resources :users, only: [:edit, :show]
+  resources :books, only: [:new, :index, :show, :create, :destroy]
+  resources :users, only: [:edit, :show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
