9e51a8bb0cc125704cd8077eb2dd16cdf8347c48
reizyte <112692614+reizyte@users.noreply.github.com>
2022-09-21 09:37:54 +0000

    icon

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ApplicationRecord
    has_one_attached :profile_image
    has_many :books, dependent: :destroy
 
-   validates :name, presence: true
+   validates :name, uniqueness: true
+   validates :name, length: {in: 2..20}
    validates :introduction, length: {maximum:50}
 
 
